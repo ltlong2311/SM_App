1e3e996c6ba88cc70b435bd39d3bda094805c038
long <ltlong2311@gmail.com>
2021-04-14 21:35:40 +0700

    upload

--- android/app/src/main/java/com/smtb/MainApplication.java
+++ android/app/src/main/java/com/smtb/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.net.Uri;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
